ad1ba03eb4462baad0780f744c9e8b3e4c533f9f
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-02-19 21:36:14 +1030

    added text to display

--- main.py
+++ main.py
@@ -6,4 +6,17 @@ window.title("Writers block block")
 canvas = Canvas(width=900, height=600, highlightthickness=0, background="white")
 canvas.pack()
 
+
+# ToDo 1. Setup the screen
+#has an input
+text = Entry(width=21, placeholder="Start Typing..")
+text.focus()
+text.place(x=400, y=0)
+
+text.bind("<FocusIn>", lambda args: text.delete('0', 'end'))
+
+# ToDo 2. Setup a 5 second timer
+# If timer = 0 set the text to empty
+# If timer = 1 srt text to red
+
 window.mainloop()
