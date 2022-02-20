206e52f0d2e81016dc357db88de0d40e8e90e08f
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-02-20 15:12:08 +1030

    updated the color of text if 1 second until game cleared

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from tkinter import *
 from tkinter import ttk
-import time
 
 window = Tk()
 window.config(padx=50, pady=50, width=900, height=700, background='white')
 window.title("Writers block block")
 
+started = False
+red = False
+
 
 def user_typing():
-    global clear_warning
+    global clear_warning, clear_game, started, red
+    if not started:
+        started = True
+    if red:
+        text.configure(foreground='black')
+
     window.after_cancel(clear_warning)
+    window.after_cancel(clear_game)
     clear_warning = window.after(4000, warning_limit)
+    clear_game = window.after(5000, game_finished)
 
 
 # ToDo 1. Setup the screen
@@ -36,16 +45,21 @@ canvas.pack()
 # ToDo 2. Setup a 5 second timer
 def game_finished():
     print("end game")
-    text.delete("1.0", 'end')
+    if started:
+        text.delete("1.0", 'end')
 
 
 # If timer = 0 set the text to empty
 # If timer = 1 srt text to red
 def warning_limit():
+    global started, red
     print("warning")
-    clear_game = window.after(1000, game_finished)
+    if started:
+        red = True
+        text.configure(foreground='red')
 
 
 clear_warning = window.after(4000, warning_limit)
+clear_game = window.after(5000, game_finished)
 
 window.mainloop()
